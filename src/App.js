cfa451126f757c7fd0d1ad110cd7185ca83e37b5
Khaled Garbaya <khaled@contentful.com>
2020-03-18 23:01:32 +0100

    Update src/App.js
    Co-Authored-By: Moe Shaaban <mshaaban0@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Information from "./components/Information";
 import BuildWebsite from "./components/BuildWebsite";
 import Additional from "./components/Additional";
 import Footer from "./components/Footer";
+import Masthead from "./components/Masthead";
 
 function App() {
   return (
