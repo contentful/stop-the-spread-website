6038c15eea8a45fe2920d82e8b9f43cf8f86e2ca
Khaled Garbaya <khaled@contentful.com>
2020-03-18 22:43:54 +0100

    fix: remove PageTitle

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
-import React from 'react';
-import Header from './components/Header';
-import Information from './components/Information';
-import BuildWebsite from './components/BuildWebsite';
-import Additional from './components/Additional';
-import Footer from './components/Footer';
-import PageTitle from './components/PageTitle';
+import React from "react";
+import Header from "./components/Header";
+import Information from "./components/Information";
+import BuildWebsite from "./components/BuildWebsite";
+import Additional from "./components/Additional";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <Header />
-      <PageTitle />
       <Information />
       <BuildWebsite />
       <Additional />
