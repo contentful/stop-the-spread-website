6c32fae0d1c110d9fad51b41a31737954d639b92
Moe Shaaban <info@mshaaban.com>
2020-03-19 12:09:34 +0100

    logo, and title

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { ReactComponent as Logo } from '../assets/logo.svg';
 
 export default () => {
   return (
     <header className="bg-white">
-      <nav className="container mx-auto" aria-label="Main Navigation">
+      <nav className="container mx-auto py-2 flex justify-between" aria-label="Main Navigation">
+        <Link className="flex items-center" to="/">
+          <Logo className="mr-2" title="Stop the spread - Covid-19" />
+          <strong>Stop the spread</strong>  
+        </Link>
         <ul className="flex justify-end">
           <li>
             <a className="block p-4 hover:text-blue-700" href="https://github.com/contentful/covid-19-site-template">Documentation</a>
