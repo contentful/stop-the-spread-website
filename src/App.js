f04fcd411c206239cde0baebdaa395290b9f9598
Moe Shaaban <info@mshaaban.com>
2020-03-26 13:11:52 +0100

    feat: react-ga

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 import { Switch, Route } from "react-router-dom";
+import withTracker from './withtraker';
 import Home from './routes/Home';
 import Faq from './routes/Faq';
 import screenshot from './assets/screenshot.png';
 
-const GA_ID = 'UA-161843176-1';
 const metaData = {
   title: 'Stop the spread',
   description: 'Create a free website for Covid-19 announcements and quickly share information about support services',
@@ -30,23 +30,10 @@ function App() {
         <meta name="twitter:description" content={metaData.description} />
         <meta name="twitter:image" content={metaData.image} />
         <meta name="twitter:card" content="summary_large_image" />
-        <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}></script>
-        <script>
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', ${GA_ID});
-          `}
-        </script>
       </Helmet>
       <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/faq">
-          <Faq />
-        </Route>
+        <Route exact path="/" component={withTracker(Home)} />
+        <Route path="/faq" component={withTracker(Faq)} />
       </Switch>
 
     </>
